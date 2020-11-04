b71a46a878839d3d5165080aad155f5910ec43bc
Gergely Mezei <mezei.gergely89@gmail.com>
2020-11-04 14:47:18 +0100

    added file_get_contents function

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html>
 
 <body>
     <h2 style="text-align: center">Getting Data from URL</h2>
-    <form align="center">
-        <label for="url">URL:</label><br>
-        <input type="text" id="url" name="url" value=""><br>
-        <input type="submit" value="Submit">
-    </form>
+    <div>
+        <form align="center" method="GET" name="getUrl">
+            <label for="url">URL:</label><br>
+            <input type="text" id="url" name="url" value=""><br>
+            <input type="submit" value="Submit" name="submit">
+        </form>
+    </div>
+    <div style="text-align: center">
+        <?php
+        if (isset($_GET['submit'])) {
+            $url = $_GET['url'];
+            $data = file_get_contents($url);
+        }
+
+        $exploded_data = explode(" ", $data);
+
+        foreach ($exploded_data as $a) {
+            if (strpos($a, "a") || strpos($a, "A")) {
+                echo $a . ",";
+            }
+        }
+
+        ?>
+    </div>
 </body>
 
 </html>
