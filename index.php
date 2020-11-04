7232b9b578939ad2592d89745d9aba53f37ed0b2
Gergely Mezei <mezei.gergely89@gmail.com>
2020-11-04 12:05:17 +0100

    added html form

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h2 style="text-align: center">Getting Data from URL</h2>
+    <form align="center">
+        <label for="url">URL:</label><br>
+        <input type="text" id="url" name="url" value=""><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
